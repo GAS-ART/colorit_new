9cd0eb44b00f5bd96ebb6bee4bfd05afab7974e2
GAS-ART <temoha1386@gmail.com>
2023-07-12 14:13:05 +0300

    Edded sub menu to header

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -87,8 +87,22 @@
          <div class="header__container">
             <nav class="header-menu">
                <ul class="header-menu__list">
-                  <li class="header-menu__list-item"><a href="#"
-                        class="header-menu__list-link">@lang('main.menu.signboards')</a></li>
+                  <li class="header-menu__list-item">
+                     <h3 class="header-menu__title">@lang('main.menu.signboards')</h3>
+                     <ul class="header-menu__sub-list">
+                        <li class="header-menu__sub-list-item">
+                           <a href="{{ route('letters', ['locale' => __('lang.current')]) }}"
+                              class="header-menu__sub-list-link">
+                              @lang('main.menu.letters.whithout_light')
+                           </a>
+                        </li>
+                        <li class="header-menu__sub-list-item">
+                           <a href="#" class="header-menu__sub-list-link">
+                              @lang('main.menu.letters.whith_light')
+                           </a>
+                        </li>
+                     </ul>
+                  </li>
                   <li class="header-menu__list-item">
                      <h3 class="header-menu__title">@lang('main.menu.letters.title')
                      </h3>
@@ -106,8 +120,22 @@
                         </li>
                      </ul>
                   </li>
-                  <li class="header-menu__list-item"><a href="#"
-                        class="header-menu__list-link">@lang('main.menu.vinyl')</a></li>
+                  <li class="header-menu__list-item">
+                     <h3 class="header-menu__title">@lang('main.menu.vinyl')</h3>
+                     <ul class="header-menu__sub-list">
+                        <li class="header-menu__sub-list-item">
+                           <a href="{{ route('letters', ['locale' => __('lang.current')]) }}"
+                              class="header-menu__sub-list-link">
+                              @lang('main.menu.letters.whithout_light')
+                           </a>
+                        </li>
+                        <li class="header-menu__sub-list-item">
+                           <a href="#" class="header-menu__sub-list-link">
+                              @lang('main.menu.letters.whith_light')
+                           </a>
+                        </li>
+                     </ul>
+                  </li>
                   <li class="header-menu__list-item"><a href="#"
                         class="header-menu__list-link">@lang('main.menu.events')</a></li>
                </ul>
