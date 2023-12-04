4cdcd4c02f7a8bc6b3b46c86e917d363d348a5a5
GAS-ART <temoha1386@gmail.com>
2023-12-04 15:12:17 +0200

    Added portfolio page

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,13 +1,22 @@
 @extends('layout')
 @section('title', __('titles.letters_whithout_light.title'))
 @section('description', __('titles.letters_whithout_light.description'))
-@section('ogTitle' ,__('titles.letters_whithout_light.ogTitle'))
+@section('ogTitle', __('titles.letters_whithout_light.ogTitle'))
 @section('ogDescription', __('titles.letters_whithout_light.ogDescription'))
 @section('page', 'letters')
 @section('mainPopupPayload', 'ПОРТФОЛИО (' . url()->current() . ')')
 
 @section('content')
-<section style="text-align: center; font-size: 60px; font-weight:700; color: #89368E;" class="test__container">
-  ПОРТФОЛИО
-</section>
+
+    <section style="text-align: center; font-size: 60px; font-weight:700; color: #89368E;" class="test__container">
+        ПОРТФОЛИО
+    </section>
+
+    <section class="portfolio-page">
+        <div class="portfolio-page__container">
+            <div class="portfolio-page__body">
+
+            </div>
+        </div>
+    </section>
 @endsection
